3880bcb5eca27196ba7b99f125b371e4bd99718f
Hugo Nogueira <nogueiradeoliveira@gmail.com>
2020-11-20 17:12:16 -0300

    proximos videos

--- src/emails/emails.service.spec.ts
+++ src/emails/emails.service.spec.ts
@@ -1,3 +1,4 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { EmailsService } from './emails.service';
@@ -9,13 +10,20 @@ describe('EmailsService', () => {
   let service: EmailsService;
   let repository;
 
+  const repositoryFactory = {
+    save: jest.fn(),
+    find: jest.fn(),
+    findOne: jest.fn(),
+    findOneAndDelete: jest.fn()
+  }
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         EmailsService,
         {
           provide: getRepositoryToken(Email),
-          useFactory: () => ({ save: jest.fn(), find: jest.fn() }),
+          useFactory: () => repositoryFactory,
         },
       ],
     }).compile();
@@ -76,14 +84,67 @@ describe('EmailsService', () => {
   });
 
   describe('findOne()', () => {
-    it('should be called repository.findOne', async () => {
+    it('should be called repository.findOne with correct email', async () => {
       await service.findOne(mockData.email);
-      expect(repository.findOne).toBeCalled();
+      expect(repository.findOne).toBeCalledWith({ email: mockData.email });
     });
 
-    it('should be returns repository.find return', async () => {
-      (repository.find as jest.Mock).mockReturnValue([mockData]);
-      expect(await service.findAll()).toEqual([mockData]);
+    it('should be returns repository.findOne return', async () => {
+      (repository.findOne as jest.Mock).mockReturnValue(mockData);
+      expect(await service.findOne(mockData.email)).toEqual(mockData);
+    });
+  });
+
+  describe('update()', () => {
+    it('should be called repository.findOne with correct email', async () => {
+      (repository.findOne as jest.Mock).mockReturnValue(mockData);
+      await service.update(mockData);
+      expect(repository.findOne).toBeCalledWith({ email: mockData.email });
+    });
+
+    it('should be throw if email not exists', async () => {
+      (repository.findOne as jest.Mock).mockReturnValue(undefined);
+      expect(service.update(mockData)).rejects.toThrow(new NotFoundException());
+    });
+
+    it('should be throw if called with empty name', async () => {
+      let error = false;
+      try {
+        await service.create({ ...mockData, name: '' });
+      } catch (err) {
+        error = err;
+      }
+      expect(error[0].property).toBe('name');
+    });
+
+    it('should be called repository.save and returns updated object', async () => {
+      const mockUpdatedData = { ...mockData, name: 'updated name' };
+      (repository.findOne as jest.Mock).mockReturnValue(mockData);
+      (repository.save as jest.Mock).mockReturnValue(mockUpdatedData);
+      expect(await service.update(mockUpdatedData)).toEqual(mockUpdatedData);
+      expect(repository.save).toBeCalledWith(mockUpdatedData);
+    });
+  });
+
+  describe('remove()', () => {
+    it('should be called repository.findOneAndDelete with correct email', async () => {
+      (repository.findOneAndDelete as jest.Mock).mockReturnValue({
+        value: mockData,
+      });
+      await service.remove(mockData.email);
+      expect(repository.findOneAndDelete).toBeCalledWith({ email: mockData.email });
+    });
+
+    it('should be throw if email not exists', async () => {
+      (repository.findOneAndDelete as jest.Mock).mockReturnValue({});
+      expect(service.remove(mockData.email)).rejects.toThrow(new NotFoundException());
+    });
+
+    it('should be returns removed object', async () => {
+      (repository.findOneAndDelete as jest.Mock).mockReturnValue({
+        value: mockData,
+      });
+      expect(await service.remove(mockData.email)).toEqual(mockData);
     });
   });
 });
